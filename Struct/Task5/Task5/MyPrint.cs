97187657e49a88830682787883868249ba1e9395
Danil <d.gorbach.step@gmail.com>
2020-01-09 11:36:49 +0200

    Add Task5

---
+++ Struct/Task5/Task5/MyPrint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task5
+{
+    static class MyPrint
+    {
+        public static void Print(string stroka, int color)
+        {
+            var colors = (Colors) color;
+            switch (colors)
+            {
+                case Colors.Blue:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(stroka);
+                    break;
+                case Colors.Green:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(stroka);
+                    break;
+                case Colors.Magenta:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(stroka);
+                    break;
+                case Colors.Red:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(stroka);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Struct/Task5/Task5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task5
+{
+    enum Colors
+    {
+        Red = 1,
+        Blue,
+        Green,
+        Magenta,
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String:");
+            var input = Console.ReadLine();
+            Console.WriteLine("Chosen color 1-Red, 2-Blue, 3-Green, 4-Magneta");
+            var numOfColor = Convert.ToInt32(Console.ReadLine());
+            if (numOfColor < 1 || numOfColor > 8) { Console.WriteLine("Wrong input!"); }
+            else
+            {
+                Console.WriteLine("Result: ");
+                MyPrint.Print(input, numOfColor);
+            }
+            Console.ReadKey();
+        }
+    }
+}
