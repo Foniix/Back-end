b6be211f682966a6158b36e02669670aef6c4345
Danil <d.gorbach.step@gmail.com>
2020-01-09 10:58:45 +0200

    Add Struct + Task1

---
+++ Struct/Task1/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task1
+{
+    struct Notebook
+    {
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public int Price { get; set; }
+
+        public Notebook(string model, string manufacturer, int price)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Model = {Model};\nManufacturer = {Manufacturer};\nPrice = {Price}$.\n";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var notebook = new Notebook("MacBook", "Pro", 5000);
+
+            Console.WriteLine(notebook.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
