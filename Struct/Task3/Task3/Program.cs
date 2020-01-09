cf6c532474ec70a65aad92082bbbdf82f73e9e39
Danil <d.gorbach.step@gmail.com>
2020-01-09 11:17:46 +0200

    Add Task3

---
+++ Struct/Task3/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "Class Change";
+        }
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "Struct Change";
+        }
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+            var myStruct = new MyStruct();
+            myClass.change = "Class doesn't Change";
+            myStruct.change = "Struct doesn't Change";
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+            Console.WriteLine($"Struct: {myStruct.change}\nClass: {myClass.change}");
+            Console.ReadKey();
+        }
+    }
+}
