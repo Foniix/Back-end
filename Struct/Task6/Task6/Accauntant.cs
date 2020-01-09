a81cbfc7f809fa96c1d72ba839d3b36f932f452f
Danil <d.gorbach.step@gmail.com>
2020-01-09 11:55:19 +0200

    Add Task6

---
+++ Struct/Task6/Task6/Accauntant.cs
@@ -0,0 +1,10 @@
+﻿namespace Task6
+{
+    class Accauntant
+    {
+        public bool AskForBonus(Position worker, int hours)
+        {
+            return (int)worker < hours;
+        }
+    }
+}
---
+++ Struct/Task6/Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task6
+{
+    enum Position
+    {
+        Junior = 10, 
+        Middle = 20, 
+        Senior = 40
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accauntant acc = new Accauntant();
+            var senior = Position.Senior;
+            Console.WriteLine(acc.AskForBonus(senior, 50) ? "Take premium" : "Don`t take premium");
+            Console.ReadKey();
+        }
+    }
+}
