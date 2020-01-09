e15ab43edcc55211680b2a9e778078e8c1f54485
Danil <d.gorbach.step@gmail.com>
2020-01-09 10:05:33 +0200

    Add Static

---
+++ Static/Task1/Task1/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    static class Calculator
+    {
+        public static double Add(double i, double j)
+        {
+            return i + j;
+        }
+
+        public static double Sub(double i, double j)
+        {
+            return i - j;
+        }
+
+        public static double Div(double i, double j)
+        {
+            return i / j;
+        }
+
+        public static double Mul(double i, double j)
+        {
+            return i * j;
+        }
+    }
+}
