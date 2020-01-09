1662e4fd86df42151e90d9226420ea2967daf990
Danil <d.gorbach.step@gmail.com>
2020-01-09 10:45:01 +0200

    Add Task2-3

---
+++ Static/Task2_3/Task2_3/ArrayExtension.cs
@@ -0,0 +1,80 @@
+﻿namespace Task2_3
+{
+    static class ArrayExtension
+    {
+        public static void SortAscending(this int[] arr, int first, int last)
+        {
+            while (true)
+            {
+                if (first < last)
+                {
+                    var middle = arr[(first + last) / 2];
+                    int i = first, j = last;
+                    while (i <= j)
+                    {
+                        while (i <= last && arr[i] < middle) ++i;
+                        while (j >= first && arr[j] > middle) --j;
+                        if (i > j) continue;
+                        var temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        ++i;
+                        --j;
+                    }
+
+                    if (i < last) SortAscending(arr, i, last);
+                    if (j > first)
+                    {
+                        last = j;
+                        continue;
+                    }
+                }
+
+                break;
+            }
+        }
+
+        public static void SortByDecreasing(this int[] arr, int first, int last)
+        {
+            while (true)
+            {
+                if (first < last)
+                {
+                    var middle = arr[(first + last) / 2];
+                    int i = first, j = last;
+                    while (i <= j)
+                    {
+                        while (i <= last && arr[i] > middle) ++i;
+                        while (j >= first && arr[j] < middle) --j;
+                        if (i > j) continue;
+                        var temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        ++i;
+                        --j;
+                    }
+
+                    if (i < last) SortByDecreasing(arr, i, last);
+                    if (j > first)
+                    {
+                        last = j;
+                        continue;
+                    }
+                }
+
+                break;
+            }
+        }
+
+        public static void SortByBool(this int[] arr, int first, int last, bool way)
+        {
+            if (way)
+                SortAscending(arr, first, last);
+            else
+            {
+                SortByDecreasing(arr, first, last);
+            }
+        }
+
+    }
+}
---
+++ Static/Task2_3/Task2_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[20];
+            var rnd = new Random();
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(0,100);
+            }
+            Console.WriteLine("Массив перед сортировкой:");
+            foreach (var a in arr)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("По возрастани - 0,\nПо убыванию - 1.");
+            var value = Console.ReadLine();
+            if (value == "0")
+            {
+                arr.SortByBool(0, arr.Length - 1, true);
+                foreach (var a in arr)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+            if (value == "1")
+            {
+                arr.SortByBool(0, arr.Length - 1, false);
+                foreach (var a in arr)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+            else
+                Console.WriteLine("Error value!");
+
+            Console.ReadKey();
+        }
+    }
+}
