7e375c832e3cde679236cb041e0e935cfdc27469
Danil <d.gorbach.step@gmail.com>
2020-01-09 10:51:16 +0200

    Add Task4

---
+++ Static/Task4/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string str = "Hello from Miami!";
+            Console.WriteLine(str.MySubstring(2, 7));
+            Console.WriteLine(str.MyIndexOf("from"));
+            Console.WriteLine(str.MyReplace("i", "O"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Static/Task4/Task4/StringExtension.cs
@@ -0,0 +1,30 @@
+﻿namespace Task4
+{
+    static class StringExtension
+    {
+        public static string MySubstring(this string str, int beg, int len)
+        {
+            var res = new char[len];
+            for (var i = 0; i < len; i++)
+                res[i] = str[i + beg];
+            return new string(res);
+        }
+        public static int MyIndexOf(this string str, string value)
+        {
+            for (var i = 0; i < str.Length - value.Length + 1; i++)
+                if (str.MySubstring(i, value.Length) == value)
+                    return i;
+            return -1;
+        }
+        public static string MyReplace(this string str, string oldValue, string newValue)
+        {
+            while (str.MyIndexOf(oldValue) >= 0)
+            {
+                var i = str.MyIndexOf(oldValue);
+                str = str.Remove(i, oldValue.Length);
+                str = str.Insert(i, newValue);
+            }
+            return str;
+        }
+    }
+}
