8d1f4c131c2ad12629c53f82a872c67d6455a6c8
Danil <d.gorbach.step@gmail.com>
2020-02-06 09:34:08 +0200

    Created Delegate directory. Added Task1

---
+++ Delegate/Task1/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        delegate int Avarage(int a, int b, int c);
+        static void Main(string[] args)
+        {
+            AvarageCalculator();
+            Console.ReadLine();
+        }
+
+        static void AvarageCalculator()
+        {
+            try
+            {
+                Console.WriteLine("Bведите три числа через пробел:");
+                var numbers = Console.ReadLine().Split(' ');
+
+                Avarage avarage = (a, b, c) => { return (a + b + c) / 3; };
+
+                Console.WriteLine("Среднее арифметическое этих чисел = " + avarage(Convert.ToInt32(numbers[0]), Convert.ToInt32(numbers[1]), Convert.ToInt32(numbers[2])));
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный ввод! Попроуйте ещё раз.");
+                AvarageCalculator();
+            }
+        }
+    }
+}
