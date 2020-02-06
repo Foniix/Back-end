ec17984c3e68cce4990ea4e9aaac610303f4c0b5
Danil <d.gorbach.step@gmail.com>
2020-02-06 09:57:07 +0200

    Added Task3

---
+++ Delegate/Task3/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task3
+{
+    internal class Program
+    {
+        private delegate int Rnd();
+        private static int MyRandom()
+        {
+            var rand = new Random();
+            return rand.Next(10);
+        }
+
+        private static void Main(string[] args)
+        {
+            static double AvarageDelegate(IReadOnlyCollection<Rnd> r)
+            {
+                var sum = r.Sum(t => t.Invoke());
+
+                return sum / r.Count;
+            }
+            Console.WriteLine($"\nResult {AvarageDelegate(new Rnd[3] { MyRandom, MyRandom, MyRandom })} ");
+            Console.ReadKey();
+        }
+
+    }
+}
