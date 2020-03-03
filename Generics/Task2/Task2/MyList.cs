4fc0674f6b36ca2e86887f077c588143f2d6dc2c
Danil <d.gorbach.step@gmail.com>
2020-03-03 13:24:57 +0200

    Added Task2

---
+++ Generics/Task2/Task2/MyList.cs
@@ -0,0 +1,41 @@
+﻿namespace Task2
+{
+    internal class MyList<T>
+    {
+        public T[] MyArray;
+        public int Size { get; private set; }
+
+        public MyList()
+        {
+            Size = 0;
+            MyArray = new T[] { };
+
+        }
+        public MyList(int size, params T[] list)
+        {
+            Size = size;
+            MyArray = list;
+        }
+        public T this[int i]
+        {
+            get => MyArray[i];
+            set
+            {
+                if (i <= Size)
+                    MyArray[i] = value;
+            }
+        }
+
+        public void Add(T newElement)
+        {
+            Size++;
+            var newMyArray = new T[Size];
+            for (var i = 0; i < Size - 1; i++)
+            {
+                newMyArray[i] = MyArray[i];
+            }
+            newMyArray[Size - 1] = newElement;
+            MyArray = newMyArray;
+        }
+    }
+}
---
+++ Generics/Task2/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>(10, 1,2,3,4,5,6,7,8,9,10);
+            Console.WriteLine("List creating:");
+            for (var i = 0; i < list.Size; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+
+            list.Add(11);
+
+            Console.WriteLine("\nAdded new elements:");
+            for (var i = 0; i < list.Size; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+
+            Console.WriteLine("\nSize = " + list.Size);
+            Console.ReadKey();
+        }
+    }
+}
