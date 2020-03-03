60f47910d11e955d5d59e4375b636e8525d2d266
Danil <d.gorbach.step@gmail.com>
2020-03-03 13:41:19 +0200

    Added Task5

---
+++ Generics/Task5/Task5/CarCollection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Task5
+{
+    internal class CarCollection<T, TU>
+    {
+        public Tuple<T, TU>[] Cars { get; set; }
+        public int Size { get; private set; }
+
+
+        public CarCollection()
+        {
+            Cars = new Tuple<T, TU>[] { };
+            Size = 0;
+        }
+
+        public CarCollection(int size, params Tuple<T, TU>[] tuple)
+        {
+            Cars = tuple;
+            Size = size;
+        }
+
+        public Tuple<T, TU> this[int i]
+        {
+            get => Cars[i];
+            set => Cars[i] = value;
+        }
+
+        public void Add(T carName, TU year)
+        {
+            if (carName.GetType() != typeof(string) || year.GetType() != typeof(int)) return;
+            var temp = new Tuple<T, TU>[++Size];
+            for (var i = 0; i < Size - 1; i++)
+            {
+                temp[i] = Cars[i];
+            }
+            temp[Size - 1] = new Tuple<T, TU>(carName, year);
+            Cars = temp;
+
+
+
+        }
+
+        public void DeleteAll()
+        {
+            var temp = new Tuple<T, TU>[0];
+            Size = 0;
+            Cars = temp;
+        }
+
+    }
+}
--- Generics/Task5/Task5/Program.cs
+++ Generics/Task5/Task5/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task5
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            var car = new CarCollection<string, int>(2, new Tuple<string, int>("BMW", 2020), new Tuple<string, int>("Tesla", 2010));
+
+            car.Add("Audi", 2020);
+
+            Console.WriteLine("Before deleting:\n");
+
+            for (var i = 0; i < car.Size; i++)
+            {
+                Console.WriteLine(car[i].Item1 + " - " + car[i].Item2);
+            }
+
+            car.DeleteAll();
+
+            car.Add("Honda", 2015);
+
+            Console.WriteLine("\nAfter deleting and adding new element:");
+
+            for (var i = 0; i < car.Size; i++)
+            {
+                Console.WriteLine(car[i].Item1 + " - " + car[i].Item2);
+            }
+            Console.ReadKey();
         }
     }
 }
