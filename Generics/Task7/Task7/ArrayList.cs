4fbe543f991fe4974e13e2e7d809353f488cec0d
Danil <d.gorbach.step@gmail.com>
2020-03-03 13:51:58 +0200

    Added Task7

---
+++ Generics/Task7/Task7/ArrayList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace Task7
+{
+    internal class ArrayList
+    {
+        public object[] MyArray;
+        public int Size { get; private set; }
+
+        public ArrayList()
+        {
+            MyArray = new object[] { };
+            Size = 0;
+        }
+
+        public object this[int i]
+        {
+            get => MyArray[i];
+            set => MyArray[i] = value;
+        }
+        public ArrayList(object[] obj)
+        {
+            MyArray = obj;
+            Size = obj.Count();
+        }
+
+        public void Add(object obj)
+        {
+            Array.Resize(ref MyArray, ++Size);
+            MyArray[Size - 1] = obj;
+        }
+        public void Clear()
+        {
+            MyArray = new object[] { };
+            Size = 0;
+        }
+
+        public void Reverse()
+        {
+            var array = new object[Size];
+            var temp = MyArray;
+            var index = Size - 1;
+            var i = 0;
+            while (index >= 0)
+            {
+                array[index--] = temp[i++];
+            }
+            MyArray = array;
+
+        }
+
+        public object[] CopyTo(int from)
+        {
+            var temp = from;
+            if (temp <= 0 || temp >= Size) return null;
+            var newSize = Size - @from;
+            var to = new object[newSize];
+            for (var i = 0; i < newSize; i++)
+            {
+                to[i] = MyArray[temp];
+                temp++;
+            }
+            return to;
+
+        }
+        public void AddRange(object[] range)
+        {
+            var temp = this.Size;
+            var index = 0;
+            var tempt = Size + range.Count();
+            Array.Resize(ref MyArray, tempt);
+            Size += range.Count();
+            for (var i = temp; i < Size; i++)
+            {
+                MyArray[i] = range[index++];
+            }
+        }
+        public void Remove(int rem)
+        {
+            var index = rem;
+            for (var i = index; i < MyArray.Count() - 1; i++)
+            {
+                MyArray[i] = MyArray[i + 1];
+            }
+            Array.Resize(ref MyArray, --Size);
+        }
+    }
+}
---
+++ Generics/Task7/Task7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task7
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var arrayList = new ArrayList(new object[] { "Hi From", ' ', 2020, ' ', 'D','N','E','P','R' });
+            Console.WriteLine("ArrayList after creating:");
+
+            for (var i = 0; i < arrayList.Size; i++)
+            {
+                Console.Write(arrayList[i]);
+            }
+
+            arrayList.Clear();
+            arrayList.AddRange(new object[] { 'H', 'i', ' ', 2000, ' ', "With Love" });
+            Console.WriteLine("\nArrayList after clearing and added range:");
+
+            for (var i = 0; i < arrayList.Size; i++)
+            {
+                Console.Write(arrayList[i]);
+            }
+
+            arrayList.Remove(1);
+            Console.WriteLine("\nAfter removing element with index 1:");
+            for (var i = 0; i < arrayList.Size; i++)
+            {
+                Console.Write(arrayList[i]);
+            }
+
+            arrayList.Reverse();
+            Console.WriteLine("\nAfter reversing:");
+            for (var i = 0; i < arrayList.Size; i++)
+            {
+                Console.Write(arrayList[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
